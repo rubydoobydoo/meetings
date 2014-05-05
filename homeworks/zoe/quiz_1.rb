a385951239891aba351f403a9b3a0d943e6174a6
Zoi Tkt <zoe_tkt@gmail.com>
2014-05-05 21:34:04 +0200

    just a silly attempt on the 1st quiz.

---
+++ homeworks/zoe/quiz_1.rb
@@ -0,0 +1,13 @@
+# Mad Libs game! Play with the user :)
+
+puts "Gimme a funny adjective!"
+adj = gets.chomp
+
+puts "How about a body part?"
+body_part = gets.chomp
+
+puts "Finally where would you like to be???"
+place = gets.chomp
+
+puts "So here it comes.."
+puts "I went to #{place} on holidays! But I hit my #{body_part}, and was quite #{adj}.."
