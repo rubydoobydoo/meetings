ff17c96576f4a2ec42cb4278601cafb6341e9b8a
Andrei Ursan <hello@andreiursan.com>
2014-05-19 20:49:25 +0200

    adding the products into a module Rdd

--- homeworks/home02/product.rb
+++ homeworks/home02/product.rb
@@ -1,17 +1,18 @@
-class Product
+module Rdd
+  class Product
 
-  def initialize(name, price, discount=0)
-    @name = name
-    @price = price
-    @discount = discount
+    def initialize(name, price, discount=0)
+      @name = name
+      @price = price
+      @discount = discount
+    end
+
+    def name
+      @name
+    end
+
+    def price
+      @price * ( 1 - @discount )
+    end
   end
-
-  def name
-    @name
-  end
-
-  def price
-    @price * ( 1 - @discount )
-  end
-
 end
