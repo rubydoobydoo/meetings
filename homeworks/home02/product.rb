a42410297d450b4836b226c6c75f3ac872f71f93
Andrei Ursan <hello@andreiursan.com>
2014-05-19 20:23:44 +0200

    one home02 solution

---
+++ homeworks/home02/product.rb
@@ -0,0 +1,17 @@
+class Product
+
+  def initialize(name, price, discount=0)
+    @name = name
+    @price = price
+    @discount = discount
+  end
+
+  def name
+    @name
+  end
+
+  def price
+    @price * ( 1 - @discount )
+  end
+
+end
