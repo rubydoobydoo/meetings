5c26d085a36df828f13ee02bbb9145048120b84c
Zoi Tkt <zoe_tkt@gmail.com>
2014-05-22 22:10:49 +0200

    worm eating falling apples

--- homeworks/zoe/worm_n_apples/worm.rb
+++ homeworks/zoe/worm_n_apples/worm.rb
@@ -45,22 +45,61 @@ class Worm
 			image.draw @x + @width, @y, 1, -1, 1
 		end
 	end
+
+	def worm_x
+		@x
+	end
+
+	def worm_y
+		@y
+	end
 end
 
 class Apple
 	def initialize window
 		@window = window
 		@apple = Gosu::Image::new(@window, "graphic/apple_100_100.png")
+		@eat_apple = Gosu::Sample.new(@window, 
+			"sounds/Apple_Bite-Simon_Craggs-1683647397.wav")
 		# image at upper left corner
-		@x = @window.width/2 - @apple.width/2
-		@y = @window.height/3  - @apple.height/2
+		rng = Random.new(123)
+		respawn_apple
+		@num_eaten_apples= 0
+	end
+
+	def respawn_apple
+		@x = (rand()*(@window.width - @apple.width)).to_i
+		@y =  - @apple.height/2
 	end
 
 	def update
+		if @y < @window.height-100-(@apple.height/2) 
+			@y = @y +2 
+		elsif eaten
+			@eat_apple.play
+			respawn_apple
+			@num_eaten_apples += 1
+			puts "I ate it!"
+		else
+			respawn_apple
+		end
 	end
 
 	def draw
 		@apple.draw @x, @y, 1
+	end
+
+	def eaten
+		x = @window.whosmyworm.worm_x
+		y = @window.whosmyworm.worm_y
+
+	#	puts "worms #{x}, #{y}, and apples #{@x} #{@y} "
+
+		return ((@x - x).abs <(@apple.width/2) && (@y - y).abs < (@apple.height/2))
+	end
+
+	def num_eaten
+		@num_eaten_apples
 	end
 end
 
@@ -80,6 +119,7 @@ class WormGame < Gosu::Window
 		#@song.play
 		@eat_apple = Gosu::Sample.new(self, 
 			"sounds/Apple_Bite-Simon_Craggs-1683647397.wav")
+		@font = Gosu::Font.new(self, Gosu::default_font_name, 20)
 	end
 
 	def button_down id
@@ -96,8 +136,12 @@ class WormGame < Gosu::Window
 		@worm.draw
 		@apple.draw
 		@background_image.draw(0,0,0)
+		@font.draw("#{@apple.num_eaten}",self.width - 50, 20, 1, 2.5, 2.5, 0xfffff073)
+	end
+
+	def whosmyworm
+		@worm
 	end
 end
-
 
 WormGame.new.show
